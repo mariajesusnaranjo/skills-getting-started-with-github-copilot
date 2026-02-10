d7b0d0a6bd1a541248708377fde7f7d085361900
Naranjo Espuig <msno@gft.com>
2026-02-10 16:37:40 +0100

    Add hello world programs and equation generator

---
+++ csharp-helloworld/Program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace HelloWorldApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Generate equations for addition and subtraction");
+        Console.WriteLine("Enter the number of equations to generate:");
+
+    }
+}
